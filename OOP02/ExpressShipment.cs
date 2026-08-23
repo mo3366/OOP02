1e88ad4f7c99e0f36a03cb365dfe92d25a254d56
mo3366 <momen23012694@gmail.com>
2026-08-23 23:59:44 +0300

    1. Shipment Class

---
+++ OOP02/ExpressShipment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal class ExpressShipment : Shipment
+    {
+        private decimal extraFee;
+
+        public decimal ExtraFee {  
+            get{
+            
+                return extraFee;
+            }
+            set
+            {
+                if(value >= 0)
+                    extraFee = value;
+            }
+        }
+        public ExpressShipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination, decimal extraFee)
+            : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+            this.extraFee = extraFee;
+        }
+        public override decimal EstimatedCost
+        {
+            get
+            {
+                return DeliveryFee + (Weight * 5) + ExtraFee;
+            }
+        }
+
+    }
+}
